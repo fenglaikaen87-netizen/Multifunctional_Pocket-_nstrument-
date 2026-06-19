49e192a0c77d8fd9b55d2919f400f09eba75e9da
大口大口喝拿铁 <18855772924@163.com>
2026-06-19 13:08:04 +0800

    添加VGA显示和XADC工程源码

---
+++ Multifunctional_Pocket _nstrument.srcs/sources_1/ip/xadc_wiz_0/xadc_wiz_0_stub.vhdl
@@ -0,0 +1,50 @@
+-- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+-- Date        : Tue Jun  2 12:09:46 2026
+-- Host        : LAPTOP-2BTRQD7I running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub {c:/Users/dkdkhnt/fproject/Multifunctional_Pocket
+--               _nstrument/Multifunctional_Pocket _nstrument.srcs/sources_1/ip/xadc_wiz_0/xadc_wiz_0_stub.vhdl}
+-- Design      : xadc_wiz_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tcsg324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity xadc_wiz_0 is
+  Port ( 
+    daddr_in : in STD_LOGIC_VECTOR ( 6 downto 0 );
+    dclk_in : in STD_LOGIC;
+    den_in : in STD_LOGIC;
+    di_in : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    dwe_in : in STD_LOGIC;
+    reset_in : in STD_LOGIC;
+    vauxp2 : in STD_LOGIC;
+    vauxn2 : in STD_LOGIC;
+    vauxp10 : in STD_LOGIC;
+    vauxn10 : in STD_LOGIC;
+    busy_out : out STD_LOGIC;
+    channel_out : out STD_LOGIC_VECTOR ( 4 downto 0 );
+    do_out : out STD_LOGIC_VECTOR ( 15 downto 0 );
+    drdy_out : out STD_LOGIC;
+    eoc_out : out STD_LOGIC;
+    eos_out : out STD_LOGIC;
+    ot_out : out STD_LOGIC;
+    vccaux_alarm_out : out STD_LOGIC;
+    vccint_alarm_out : out STD_LOGIC;
+    user_temp_alarm_out : out STD_LOGIC;
+    alarm_out : out STD_LOGIC;
+    vp_in : in STD_LOGIC;
+    vn_in : in STD_LOGIC
+  );
+
+end xadc_wiz_0;
+
+architecture stub of xadc_wiz_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "daddr_in[6:0],dclk_in,den_in,di_in[15:0],dwe_in,reset_in,vauxp2,vauxn2,vauxp10,vauxn10,busy_out,channel_out[4:0],do_out[15:0],drdy_out,eoc_out,eos_out,ot_out,vccaux_alarm_out,vccint_alarm_out,user_temp_alarm_out,alarm_out,vp_in,vn_in";
+begin
+end;
